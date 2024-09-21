78a9736edf280e69987b529aa56b7b196557e4a5
Nga8324 <nga.nguyenthithuy@hcmut.edu.vn>
2024-09-21 09:21:25 +0700

    no message

---
+++ LAB01_exercise/Core/Inc/ex4.h
@@ -0,0 +1,16 @@
+/*
+ * ex4.h
+ *
+ *  Created on: Sep 21, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EX4_H_
+#define INC_EX4_H_
+#include "main.h"
+
+void display7SEG(int num);
+void display();
+
+#endif /* INC_EX4_H_ */
+
