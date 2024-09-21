e742d85a478ecea7e3451e9d16299fb40e1a4e98
Nga8324 <nga.nguyenthithuy@hcmut.edu.vn>
2024-09-21 22:12:38 +0700

    no message

---
+++ LAB01_exercise/Core/Src/ex7_8_9_10.c
@@ -0,0 +1,25 @@
+/*
+ * ex7_8_9_10.c
+ *
+ *  Created on: Sep 21, 2024
+ *      Author: Admin
+ */
+#ifndef INC_EX10_H_
+#define INC_EX10_H_
+
+#include "main.h"
+
+//Exercise 7
+void clearAllClock();
+
+//Exercise 8
+void setNumberOnClock(int num);
+
+//Exercise 9
+void clearNumberOnClock(int num);
+
+//void display();
+
+#endif /* INC_EX10_H_ */
+
+
