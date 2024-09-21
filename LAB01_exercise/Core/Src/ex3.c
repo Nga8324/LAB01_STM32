89ce17337aef0f4de2a35bbb22ecbde9da99dd0b
Nga8324 <nga.nguyenthithuy@hcmut.edu.vn>
2024-09-21 09:12:31 +0700

    no message

--- LAB01_exercise/Core/Src/ex3.c
+++ LAB01_exercise/Core/Src/ex3.c
@@ -14,7 +14,7 @@ int green_counter1 = 4;
 int yellow_counter1 = 3;
 int red_counter1 = 6;
 void display1(){
-	 if (green_status1 == 1){
+	if (green_status1 == 1){
 		HAL_GPIO_WritePin(Green1_GPIO_Port, Green1_Pin, SET);
 		green_counter1 --;
 
@@ -24,7 +24,7 @@ void display1(){
 			HAL_GPIO_WritePin(Green1_GPIO_Port, Green1_Pin, RESET);
 			yellow_status1 = 1;
 		}
-	 }
+	}
 	if (yellow_status1 == 1){
 		HAL_GPIO_WritePin(Yellow1_GPIO_Port, Yellow1_Pin, SET);
 		yellow_counter1 --;
@@ -59,11 +59,11 @@ int green_counter2 = 4;
 int yellow_counter2 = 3;
 int red_counter2 = 6;
 void display2(){
-	  if (red_status2 == 1){
-	  	 HAL_GPIO_WritePin(Red2_GPIO_Port, Red2_Pin, SET);
-	  	 red_counter2 --;
+	 if (red_status2 == 1){
+	  	HAL_GPIO_WritePin(Red2_GPIO_Port, Red2_Pin, SET);
+	  	red_counter2 --;
 
-	  	 if(red_counter2 == 0){
+	  	if(red_counter2 == 0){
 	  	 	red_status2 = 0;
 	  	 	red_counter2 = 5;
 	  	 	HAL_GPIO_WritePin(Red2_GPIO_Port, Red2_Pin, RESET);
@@ -83,18 +83,18 @@ void display2(){
 		  }
 	  }
 	  if (yellow_status2 == 1){
-	 		  HAL_GPIO_WritePin(Yellow2_GPIO_Port, Yellow2_Pin, SET);
-	 		  yellow_counter2 --;
+		  HAL_GPIO_WritePin(Yellow2_GPIO_Port, Yellow2_Pin, SET);
+		  yellow_counter2 --;
 
-	 		  if(yellow_counter2 == 0){
-	 			  yellow_status2 = 0;
-	 			  yellow_counter2 = 3;
-	 			  HAL_GPIO_WritePin(Yellow2_GPIO_Port, Yellow2_Pin, RESET);
+		  if(yellow_counter2 == 0){
+			  yellow_status2 = 0;
+			  yellow_counter2 = 3;
+			  HAL_GPIO_WritePin(Yellow2_GPIO_Port, Yellow2_Pin, RESET);
 
-	 			  HAL_GPIO_WritePin(Red2_GPIO_Port, Red2_Pin, SET);
-	 			  red_status2 = 1;
-	 		  }
-	 	}
+			  HAL_GPIO_WritePin(Red2_GPIO_Port, Red2_Pin, SET);
+			  red_status2 = 1;
+		  }
+	  }
 }
 
 
