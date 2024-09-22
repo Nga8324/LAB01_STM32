defe078aed99a236e0d36a00f5199691d61b0b05
Nga8324 <nga.nguyenthithuy@hcmut.edu.vn>
2024-09-22 20:38:11 +0700

    no message

--- LAB01_exercise/Core/Src/ex1.c
+++ LAB01_exercise/Core/Src/ex1.c
@@ -7,19 +7,20 @@
 
 #include "ex1.h"
 
+//Initialize the GPIO pins for two leds: Red led on, Yellow led off
 void init(){
-	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET); // Red led on
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET); // Yellow led off
 }
 
 int counter = 2;
 void display(){
 	if(counter == 0)
 	{
-		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-		HAL_GPIO_TogglePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin);
-		counter = 2;
+		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin); // Toggle red led
+		HAL_GPIO_TogglePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin); // Toggle yellow led
+		counter = 2; // Reset the counter back to 2
 	}
-	counter --;
+	counter --; // Decrement the counter
 }
 
